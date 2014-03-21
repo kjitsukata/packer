a1823cb1e509ec17265e03f7c90c123671d31edd
Kazuyuki Jitsukata <kjitsukata@gmail.com>
2014-03-21 12:57:29 +0900

    Initial commit

---
+++ debian/script/setup-base.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VAGRANT_USER=vagrant
+VAGRANT_HOME=/home/${VAGRANT_USER}
+VAGRANT_KEY_URL='https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+
+# installing packages
+echo "Installing Packages"
+sudo apt-get install -y build-essential perl dkms linux-headers-`uname -r`
+
+# installing vagrant keys
+echo "Installing Vagrant User Keys"
+mkdir -pm 700 ${VAGRANT_HOME}/.ssh
+wget --no-check-certificate ${VAGRANT_KEY_URL} -O ${VAGRANT_HOME}/.ssh/authorized_keys
+chmod 600 ${VAGRANT_HOME}/.ssh/authorized_keys
+chown -R vagrant:vagrant ${VAGRANT_HOME}/.ssh
+
