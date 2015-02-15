965ecf62b1dbf24d764d1c8d3f68dc9d256f0f24
Kazuyuki Jitsukata <kjitsukata@gmail.com>
2015-02-15 15:54:21 +0900

    [modify]vagrantユーザ公開鍵の取得先変更
    vagrantユーザ公開鍵の取得先をhttps://raw.githubusercontent.com/へ変更しました。

--- debian/script/setup-base.sh
+++ debian/script/setup-base.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-VAGRANT_USER=vagrant
+VAGRANT_USER='vagrant'
+VAGRANT_GROUP='vagrant'
 VAGRANT_HOME=/home/${VAGRANT_USER}
-VAGRANT_KEY_URL='https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+VAGRANT_KEY_URL='https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub'
 
 # installing packages
 echo "Installing Packages"
@@ -13,5 +14,5 @@ echo "Installing Vagrant User Keys"
 mkdir -pm 700 ${VAGRANT_HOME}/.ssh
 wget --no-check-certificate ${VAGRANT_KEY_URL} -O ${VAGRANT_HOME}/.ssh/authorized_keys
 chmod 600 ${VAGRANT_HOME}/.ssh/authorized_keys
-chown -R vagrant:vagrant ${VAGRANT_HOME}/.ssh
+chown -R ${VAGRANT_USER}:${VAGRANT_GROUP} ${VAGRANT_HOME}/.ssh
 
